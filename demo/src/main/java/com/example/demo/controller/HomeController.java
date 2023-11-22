3ceb0740c8084bfa5c0effcbc0dddd3d1d8fc509
Sury <sury.epm@gmail.com>
2023-11-22 17:36:55 +0900

    index

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,14 @@
-package com.example.sample.controller;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	@GetMapping(value= {"/", "/index"})
+	public String main() {
+		return "index";
+	}
 
-		@GetMapping("/home")
-		public String home() {
-			return "home";
-		}
 }
